8e2219a17f17b4b95e583eef7a11d417a96e91ae
jordan0809 <132383284+jordan0809@users.noreply.github.com>
2025-02-17 20:12:55 +0100

    Create D-Wave.py

---
+++ D-Wave.py
@@ -0,0 +1,23 @@
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+import numpy as np
+from dimod import Binary
+
+token = "insert API token"
+qpu = DWaveSampler(token=token)
+sampler = EmbeddingComposite(qpu)
+
+def dwave_solve(Q):
+  Q = Q.detach().numpy()
+  qubo = 0
+  for i in range(len(Q)):
+      qubo += Q[i,i]*Binary(i)
+      for j in range(i+1,len(Q)):
+          qubo += 2*Q[i,j]*Binary(i)*Binary(j)
+
+  sampleset = sampler.sample(qubo,num_reads=100)
+  best_sample = sampleset.first.sample
+  solution = [best_sample[i] for i in range(len(Q))]
+  energy = sampleset.first.energy
+
+  return energy,solution
